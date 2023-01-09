0f7609e8e4e5487401bb5bbd21579521dbc50f56
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-01-09 22:51:41 +0300

    Corrections

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,9 +6,7 @@ contains the MyList class
 
 class MyList(list):
     """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
+    pass
 
     def print_sorted(self):
         """prints the sorted list"""

54c6bdd5f664bbd3de2a634ed85d9ee52beb540b
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-03-14 00:55:27 +0300

    Corrections 1

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const _strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < _strings.length; i++) {
+  console.log(_strings[i]);
 }

3ceb45675d6211d06f2b26beda92dacebedc03a5
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-03 23:07:00 +0300

    Bugfix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script that fetches https://intranet.hbtn.io/status
+"""A script that fetches https://alx-intranet.hbtn.io/status
 using urlib package
 """
 
@@ -7,9 +7,9 @@ using urlib package
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body res:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status using requests package"""
+"""fetches https://alx-intranet.hbtn.io/status using requests package"""
 import requests
 
 
 if __name__ == "__main__":
-    res = requests.get("https://intranet.hbtn.io/status")
+    res = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))

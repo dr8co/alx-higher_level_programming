7875c6b12fa9cf44f686f3d8a459e0053ad1e80c
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-03 23:11:50 +0300

    Bugfix #2

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,9 @@ using urlib package
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
         content = res.read()
-        print("Body res:")
+        print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))

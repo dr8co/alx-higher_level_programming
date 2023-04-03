f9106ff0f549e5f6625755f390b8cdf50edf79d7
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-03 22:29:23 +0300

    Holberton School backend interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Lists 10 commits (from the most recent to oldest) of a repository.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    res = requests.get(url)
+    commits = res.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass

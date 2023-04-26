b142972bd80643079ee72660c45c05bda8150c72
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-26 14:31:54 +0300

    Reformat 101 for Semistandard style

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -9,23 +9,23 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        const data = JSON.parse(body).characters;
-        printChars(data, 0);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body).characters;
+    printChars(data, 0);
+  }
 });
 
-function printChars(data, index) {
-    request(data[index], function (error, response, body) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(JSON.parse(body).name);
-            if (index + 1 < data.length) {
-                printChars(data, index + 1);
-            }
-        }
-    });
+function printChars (data, index) {
+  request(data[index], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < data.length) {
+        printChars(data, index + 1);
+      }
+    }
+  });
 }

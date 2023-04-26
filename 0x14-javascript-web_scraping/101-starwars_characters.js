a674357e5bdf18f38b3c3418eac64dde0f7cb825
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-04-26 14:30:42 +0300

    Star wars characters in right order

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/*
+A script that prints all characters of a Star Wars movie:
+  The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+  Display one character name by line in the same order of the list “characters” in the /films/ response
+*/
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+    if (error) {
+        console.log(error);
+    } else {
+        const data = JSON.parse(body).characters;
+        printChars(data, 0);
+    }
+});
+
+function printChars(data, index) {
+    request(data[index], function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(JSON.parse(body).name);
+            if (index + 1 < data.length) {
+                printChars(data, index + 1);
+            }
+        }
+    });
+}

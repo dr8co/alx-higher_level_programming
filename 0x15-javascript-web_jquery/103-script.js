fc5937bbf5ed5d232060acc96b37100e0fe256a9
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-05-03 18:27:42 +0300

    Fix a typo

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -10,7 +10,7 @@ $('document').ready(function () {
 });
 
 function translate () {
-  const url = 'https://www.fourtonfish.com/hellosalut/hello/?';
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
   $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
     $('DIV#hello').html(data.hello);
   });
